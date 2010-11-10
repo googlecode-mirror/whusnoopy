680a0340d1353a6b2ce432b62b211cf205549713
whusnoopy <whusnoopy@c529cec6-0244-11de-b556-9305e745af9e>
2010-11-10 08:20:30 +0000

    A temp version. 1. Move How-To series into a folder named as howto 2. Add a page about SecureCRT

--- www/bar.php
+++ www/bar.php
@@ -4,6 +4,7 @@
 <center>
 <div id="bar">
   <a href="./">HOME</a>&nbsp;
-  <a href="win7.php">Win7</a>&nbsp;
+  <a href="howto/win7.php">Win7</a>&nbsp;
+  <a href="howto/securecrt.php">SecureCRT</a>&nbsp;
 </div>
 <div id="main" align="left">
---
+++ www/howto/securecrt.php
@@ -0,0 +1,23 @@
+<?php include("header.php") ?>
+<title>Win7/Office2010 安装使用教程</title>
+<meta name="Description" content="如何安装激活 Windows 7 和 Office 2010" />
+<?php include("bar.php") ?>
+<?php include("right.php") ?>
+
+  <div id="left">
+ <div style="width:100%">
+
+<div class="pt">本页面说明</div>
+<div class="pb">
+  <img src="img/scrt01.png"><br />
+  <img src="img/scrt02.png"><br />
+  <img src="img/scrt03.png"><br />
+  <img src="img/scrt04.png"><br />
+  <img src="img/scrt05.png"><br />
+</div>
+
+ </div> 
+  </div>
+
+<?php include("footer.php") ?>
+
