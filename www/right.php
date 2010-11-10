1e9d24f8b061fd79e837c7a4633f246a80f5af90
whusnoopy <whusnoopy@c529cec6-0244-11de-b556-9305e745af9e>
2010-11-10 09:10:52 +0000

    Update structure. Add a folder to store personal pages.

--- www/right.php
+++ www/right.php
@@ -1,8 +1,8 @@
   <div id="right"> 
 <div class="pt">Quick Link</div>
 <div class="pb">
-<li>A history snapshot of my <a href="codestyle.html">Code Style</a><br />
-<li>My <a href="blog.html">blog</a>
+<li>A history snapshot of my <a href="/personal/codestyle.html">Code Style</a><br />
+<li>My <a href="/personal/blog.html">blog</a>
 </div>
   </div>
 
