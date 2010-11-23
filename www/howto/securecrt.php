562dc6b4ab79e226ceaf2fa9f3c4b446cb23701e
whusnoopy <whusnoopy@c529cec6-0244-11de-b556-9305e745af9e>
2010-11-23 05:54:37 +0000

    1. Add [code] style 2. Add some configure about vim/bash

--- www/howto/securecrt.php
+++ www/howto/securecrt.php
@@ -7,6 +7,43 @@
   <div id="left">
  <div style="width:100%">
 
+<a name="vimrc"></a>
+<div class="pt">Vim 配置脚本 .vimrc</div>
+<div class="pb">
+<pre>根据自己喜好可以进行额外修改, 放在 ~/.vimrc 即可 :)
+vimrc 的语法规则是用双引号作为行注释开始, 类似 C/C++ 里的 // 注释
+<div class="code">"set nocompatible " 非 vi 兼容模式
+syntax on " 色彩高亮
+set number " 显示行数
+set ruler " 显示当前位置于右下角
+set backspace=2 " 设置 backspace 模式为标准
+set showmatch " 显示配对括号
+set incsearch " 增量查找
+set hlsearch
+set ai " 自动缩进
+set si " 智能缩进
+set cindent " C 风格缩进
+set tabstop=4 " Tab 宽度
+set softtabstop=4 " Tab 宽度
+set shiftwidth=4 " Tab 宽度
+set expandtab " 输入的 tab(\t) 均不保持为 tab 而转换为空格
+set fileencodings=ucs-bom,utf-8,cp936,gb18030,big5,euc-jp,sjis,euc-kr,ucs-2le,latin1 "字符编码
+set statusline=%F%m%r%h%w\ [FORMAT=%{&ff}]\ [TYPE=%Y]\ [POS=%04l,%04v][%p%%]\ [LEN=%L] " 状态栏格式
+set laststatus=2 " 一直显示状态栏
+
+" 插入模式切换
+map <F9> :set paste!<BAr>set paste?<CR>
+
+" 多 Tab 时翻页
+map <silent> <F11> :tabprevious<CR>
+imap <silent> <F11> <Esc>:tabprevious<CR>gi
+
+map <silent> <F12> :tabnext<CR>
+imap <silent> <F12> <Esc>:tabnext<CR>gi
+</div>
+</pre>
+</div>
+
 <div class="pt">SecureCRT 图文教学</div>
 <div class="pb">
   以下为全文图片, 里面涉及的链接为
--- www/index.php
+++ www/index.php
@@ -13,9 +13,38 @@
 本站为 FCR (Rank) 组内临时服务器, ftp 可以通过 <a href="ftp://yewen.us" target="_blank">ftp://yewen.us</a> 或 <a href="http://yewen.us/ftp/" target="_blank">http://yewen.us/ftp/</a> 访问
 </div>
 
+<a name="update_vim"></a>
+<div class="pt"><span class="pno">WORK</span> 测试机更新 vim 版本<span class="time">2010-11-23 13:10</span></div>
+<div class="pb">
+<pre>
+去 vim 的官网下载 <a href="ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2">vim7.3 源码</a>, 解压后在目录里执行
+<div class="code">./configure --prefix=/home/work/local/vim
+make
+make install</div>
+完了后, 会有 ~/local/vim/bin/ 这个目录, 然后在 .bashrc 里加这么两行
+<div class="code">alias vim="/home/work/local/vim/bin/vim"
+alias vimdiff="/home/work/local/vim/bin/vimdiff"</div>
+或者最简单的方法, fcr 的同学, 直接从 ai-fcr-test06 把 ~/local/ 整个目录 scp -r 到自己机器上同目录即可
+该目录包含 vim 和 python 的更新
+bash 脚本也可以拷贝同一台机器上的 .bashrc, 同时强力推荐该机器上的 .toprc .vimrc .screenrc 等配置
+<div class="code">scp -r work@ai-fcr-test06.ai01:~/local/ ~/
+scp work@ai-fcr-test06.ai01:~/.bashrc ~/
+scp work@ai-fcr-test06.ai01:~/.toprc ~/
+scp work@ai-fcr-test06.ai01:~/.screenrc ~/
+scp work@ai-fcr-test06.ai01:~/.vimrc ~/</div>
+<pre>
+</div>
+
 <div class="pt">[BUZZ] 界面风格扩展为适应 1280px 宽度<span class="time">2010-11-19 20:07</span></div>
 <div class="pb">
-考虑到现在公司内最低的分辨率大多都是 X201i 的 1280*800, 所以扩展本站适应宽度为 1280px.
+考虑到现在公司内最低的分辨率大多都是 X201i 的 1280*800, 所以扩展本站适应宽度为 1280px.<br />
+<br />
+CSS 浏览器兼容现状:
+<ul>
+ <li>测试支持: Chrome, IE8+
+ <li>没有完整测试: Firefox
+ <li>明确不支持: IE6
+</ul>
 </div>
 
 <div class="pt">[WORK] 已更新 SecureCRT 使用教程<span class="time">2010-11-10</span></div>
