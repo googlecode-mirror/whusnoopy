6212cc25c4d065966dd5d0a71cab1ba377dca259
whusnoopy <whusnoopy@gmail.com>
2011-11-01 08:59:20 +0000

    finish the train process in trainModel by Gradient descent

--- machine_learning/run_auc.sh
+++ machine_learning/run_auc.sh
@@ -3,5 +3,6 @@
 ./calc_auc/calcAuc \
         --root_dir="./" \
         --logtostderr \
-        --instance_file="./data/instance_for_eval.txt" \
         --model_file="./output/model.txt" \
+        --instance_file="./data/instance_for_train.txt" \
+#        --instance_file="./data/instance_for_eval.txt" \
--- machine_learning/train/trainModel.cc
+++ machine_learning/train/trainModel.cc
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "base/flags.h"
 #include "base/logging.h"
@@ -20,12 +21,16 @@ DEFINE_OPTIONAL_FLAGS(double, theta, 1.0, \
         "the learning rate in LR");
 DEFINE_OPTIONAL_FLAGS(double, c, 2.0, \
         "the regularization factor");
+DEFINE_OPTIONAL_FLAGS(int, max_round, 100, \
+        "max round the training program runs");
 DEFINE_OPTIONAL_FLAGS(string, instance_file, \
         "./data/instance_for_training.txt", \
         "the instance file for training");
 DEFINE_OPTIONAL_FLAGS(string, model_file, \
         "./output/model.txt", \
         "the output model file");
+DEFINE_OPTIONAL_FLAGS(int, self_beta0, 0, \
+        "is the beta0 feature include already");
 
 const int MAX_LINE_LEN = 1024000;
 
@@ -37,7 +42,7 @@ struct InsNode {
     int feature[MAX_INS_FEA];
 };
 InsNode g_ins[MAX_INS_NUM];
-
+int g_sum_ins_num;
 int g_sum_fea_num;
 
 const int MAX_FEA_NUM = 10000000;
@@ -89,18 +94,86 @@ int loadInstance() {
 
         ++ins_id;
     }
+    g_sum_ins_num = ins_id;
 
     fclose(fp);
 
+    LOG(INFO) << "finish load instance file with [" << g_sum_ins_num \
+        << "] instance and [" << (g_sum_fea_num + 1 - FLAGS_self_beta0) \
+        << "] featues";
+
     return 0;
 }
 
-int nextRound() {
+double logisticTrans(double weight) {
+    return (1.0 / (1 + exp(-weight)));
+}
+
+// Gradient Descent
+int nextRoundGradient() {
+    double zi = 0;
+    double gi = 0;
+
+    for (int i = 0; i <= g_sum_ins_num; ++i) {
+        zi = g_w[g_curr_w][0];
+        for (int j = 0; j < g_ins[i].fea_num; ++j) {
+            zi += g_w[g_curr_w][g_ins[i].feature[j]];
+        }
+        if (g_ins[i].times > 0) {
+            gi = logisticTrans(-zi);
+        } else {
+            gi = logisticTrans(zi);
+        }
+        if (!FLAGS_self_beta0) {
+            g_w[g_curr_w][0] += FLAGS_theta * g_ins[i].times * gi;
+        }
+        for (int j = 0; j < g_ins[i].fea_num; ++j) {
+            g_w[g_curr_w][g_ins[i].feature[j]] +=
+                FLAGS_theta * g_ins[i].times * gi;
+        }
+    }
+
+    // regularization
+    int last_w = 1 - g_curr_w;
+    for (int k = FLAGS_self_beta0; k <= g_sum_fea_num; ++k) {
+        g_w[g_curr_w][k] -= FLAGS_theta * FLAGS_c * g_w[last_w][k];
+    }
+
     return 0;
 }
 
-double calcDiff() {
+int nextRoundNewton() {
+    // TODO: get the Hessian matrix and get the [H]^-1
+    //       seems like 1G memory can support 10K features only?
+
+    // TODO: update the g_w{t+1}
+
     return 0;
+}
+
+double nextRound() {
+    // init this round
+    int last_w = 1 - g_curr_w;
+    for (int k = FLAGS_self_beta0; k <= g_sum_fea_num; ++k) {
+        g_w[g_curr_w][k] = g_w[last_w][k];
+    }
+
+    nextRoundGradient();
+    // nextRoundNewton();
+
+    // calc diff
+    double diff = 0;
+    double sum = 0;
+    for (int i = FLAGS_self_beta0; i <= g_sum_fea_num; ++i) {
+        diff += fabs(g_w[g_curr_w][i] - g_w[last_w][i]);
+        sum += fabs(g_w[g_curr_w][i]);
+        LOG(DEBUG) << "    Feature " << i << " value: " << g_w[g_curr_w][i]
+            << ". diff: " << fabs(g_w[g_curr_w][i] - g_w[last_w][i]);
+    }
+
+    LOG(DEBUG) << "sum of |w|: " << sum;
+
+    return diff;
 }
 
 int trainModel() {
@@ -109,20 +182,19 @@ int trainModel() {
     int round = 0;
     double diff = 0;
     g_curr_w = 0;
-    while (less_times < FLAGS_stop_times) {
+    while (less_times < FLAGS_stop_times && round < FLAGS_max_round) {
         g_curr_w = 1 - g_curr_w;
         ++round;
 
-        nextRound();
-
-        diff = calcDiff();
-        LOG(INFO) << "Round: " << round << ", diff = " << diff;
+        diff = nextRound();
 
         if (diff > FLAGS_eps) {
             less_times = 0;
         } else {
             ++less_times;
         }
+        LOG(INFO) << "Round: " << round << ", diff = " << diff \
+            << ", less_times = " << less_times;
     }
 
     return 0;
@@ -138,13 +210,14 @@ int outputModel() {
         return -1;
     }
 
-    for (int i = 1; i <= g_sum_fea_num; ++i) {
+    for (int i = FLAGS_self_beta0; i <= g_sum_fea_num; ++i) {
         fprintf(fp, "%d\t%f\n", i, g_w[g_curr_w][i]);
     }
 
     fclose(fp);
 
-    LOG(INFO) << "finish output model with [" << g_sum_fea_num \
+    LOG(INFO) << "finish output model with [" \
+        << (g_sum_fea_num + 1 - FLAGS_self_beta0) \
         << "] features, model file = " << FLAGS_model_file;
 
     return 0;
